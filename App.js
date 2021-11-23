4f32448af380c23e787bffb151464c2ad4a78d01
correaa1 <bruno_correa_ds@hotmail.com>
2021-11-22 22:51:09 -0300

    Botões de ação, sem rota por enquanto

--- App.js
+++ App.js
@@ -1,58 +1,33 @@
-import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
-import { BarCodeScanner } from "expo-barcode-scanner";
+import * as React from "react";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 
-export default function App() {
-    const [hasPermission, setHasPermission] = useState(null);
-    const [scanned, setScanned] = useState(false);
-
-    useEffect(() => {
-        (async () => {
-            const { status } = await BarCodeScanner.requestPermissionsAsync();
-            setHasPermission(status === "granted");
-        })();
-    }, []);
-
-    const handleBarCodeScanned = async ({ type, data }) => {
-        setScanned(true);
-        try {
-            const requestOptions = JSON.stringify({
-                method: "POST",
-                headers: {
-                    "api-key": "100216A23C5AEE390338BBD19EA86D29",
-                    "Content-Type": "application/json",
-                },
-                body: {
-                    search: "9788551002490",
-                },
-            });
-
-            const response = await fetch("https://isbn-search-br.search.windows.net/indexes/isbn-index/docs/search?api-version=2016-09-01", requestOptions);
-            const data = await response.json();
-        } catch (err) {}
-
-        alert(`O código de barras ${data} foi escaneado! `);
-    };
-
-    if (hasPermission === null) {
-        return <Text>Requesting for camera permission</Text>;
-    }
-    if (hasPermission === false) {
-        return <Text>No access to camera</Text>;
-    }
-
+function HomeScreen() {
     return (
-        <View style={styles.container}>
-            <BarCodeScanner onBarCodeScanned={scanned ? undefined : handleBarCodeScanned} style={StyleSheet.absoluteFillObject} />
-            {scanned && <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />}
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Text>Home!</Text>
         </View>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: "column",
-        justifyContent: "center",
-    },
-});
+function SettingsScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Text>Settings!</Text>
+        </View>
+    );
+}
+
+const Tab = createMaterialBottomTabNavigator();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name="Código de barras" component={SettingsScreen} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
---
+++ Camera/index.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+
+export default function Camera() {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === "granted");
+        })();
+    }, []);
+
+    const handleBarCodeScanned = async ({ type, data }) => {
+        setScanned(true);
+        try {
+            const requestOptions = JSON.stringify({
+                method: "POST",
+                headers: {
+                    "api-key": "100216A23C5AEE390338BBD19EA86D29",
+                    "Content-Type": "application/json",
+                },
+                body: {
+                    search: "9788551002490",
+                },
+            });
+
+            const response = await fetch("https://isbn-search-br.search.windows.net/indexes/isbn-index/docs/search?api-version=2016-09-01", requestOptions);
+            const data = await response.json();
+            console.log(data);
+        } catch (err) {}
+
+        alert(`O código de barras ${data} foi escaneado! `);
+    };
+
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
+    return (
+        <View style={styles.container}>
+            <BarCodeScanner onBarCodeScanned={scanned ? undefined : handleBarCodeScanned} style={StyleSheet.absoluteFillObject} />
+            {scanned && <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "center",
+    },
+});
